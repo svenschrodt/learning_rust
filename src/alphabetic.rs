25f27480cb4c4efc40c0d85089c0174910074a1b
sven@schrodt-service.net <sven@schrodt-service.net>
2020-08-13 18:45:02 +0200

    Minor changes, adding alphabetic.rs

---
+++ src/alphabetic.rs
@@ -0,0 +1,44 @@
+/**
+ * Public module testing usage of alphabetic (alphanumeric data) types in Rust
+ *  
+ *  - Character type (char)
+ *  - String
+ * 
+ * 
+ * General: 
+ * 
+ * Character data type in Rust is the most primitive alphabetic type representing a (unicode) literal of a single character  
+ * 
+ * String data type in Rust can be classified as:
+ * 
+ *   - String Literal (&str) or
+ *   - String Object (String)
+ *
+ * 
+ * @package learning_rust
+ * @version 0.1
+ * @since 2020-08-11
+ * @author Sven Schrodt<sven@schrodt-service.net>
+ * @see https://github.com/svenschrodt/https://github.com/svenschrodt/learning_rust
+ * @see https://travis-ci.org/github/svenschrodt/https://github.com/svenschrodt/learning_rust
+ * @copyright Sven Schrodt <sven@schrodt-service.net>
+ */
+
+
+ pub fn run() {
+
+    // Character type exmples
+
+    // Defining a character --> note the single quotes
+   
+   
+    let why = 'w';
+    let at = '@';
+    // Definiing a UNICODE character by its literal
+    let savoring_food =23 '😋';
+    // Defining a character by its UNICODE { U+1F923 => 'rolling on the floor laughing'}
+    let rotfl = '\u{1F923}';
+
+
+
+ }
